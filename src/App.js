bdd3be3b73ba0175cbd5ae8d744b8709353289ba
u-Hoshi <yuta000707@yahoo.co.jp>
2020-12-21 18:49:56 +0900

    Roomコンポーネントを作成し、Appコンポーネントで呼び出した。

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Login from './Login'
+import SignUp from './SignUp'
+import Sign from './Room'
+
+const App = () => {
+    return (
+        <>
+            <Login />
+            <SignUp />
+            <Room />
+        </>
+    )
+}
+export default App
